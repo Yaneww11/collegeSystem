5772190234349acfe4d215d8bd2c57cc42f4b415
Yaneww11 <yaneyanev2807@gmail.com>
2025-06-18 23:40:09 +0300

    fix: check

--- collegeSystem/main_app/templatetags/is_teacher.py
+++ collegeSystem/main_app/templatetags/is_teacher.py
@@ -5,4 +5,8 @@ register = template.Library()
 
 @register.filter(name='is_teacher')
 def is_teacher(user):
-    return hasattr(user.profile, 'teacher_profile') or user.is_superuser
+    if user.is_superuser:
+        return True
+    if not hasattr(user, 'profile'):
+        return False
+    return hasattr(user.profile, 'teacher_profile')
